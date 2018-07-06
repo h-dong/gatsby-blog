b9ae8567558544d5f300b301b802a692f23757a2
Hao Dong <dong_hao@hotmail.co.uk>
2018-07-06 02:31:59 +0100

    Fixed bug where all posts link were pointing to page/xyz

--- src/templates/blogs.js
+++ src/templates/blogs.js
@@ -4,22 +4,20 @@ import ChevronRight from 'react-icons/lib/md/chevron-right'
 import Calendar from 'react-icons/lib/fa/calendar'
 import Pagination from '../components/pagination/Pagination'
 
-const BlogPost = ({ node }) => {
-    return (
-        <article className="card">
-            <h1 className="card-title">
-                <Link to={node.slug}>{node.title}</Link>
-            </h1>
-            <div className="card-meta"><Calendar /> <span>{node.publishDate}</span></div>
-            <div className="card-content" lang="en" dangerouslySetInnerHTML={{ __html: node.description.childMarkdownRemark.html }} />
-            <div className="card-action">
-                <Link to={node.slug}>
-                    Read More <ChevronRight />
-                </Link>
-            </div>
-        </article>
-    )
-}
+const BlogPost = ({ node }) => (
+    <article className="card">
+        <h1 className="card-title">
+            <Link to={`/${node.slug}`}>{node.title}</Link>
+        </h1>
+        <div className="card-meta"><Calendar /> <span>{node.publishDate}</span></div>
+        <div className="card-content" lang="en" dangerouslySetInnerHTML={{ __html: node.description.childMarkdownRemark.html }} />
+        <div className="card-action">
+            <Link to={node.slug}>
+                Read More <ChevronRight />
+            </Link>
+        </div>
+    </article>
+)
 
 const Blogs = ({ pathContext }) => {
     const { group, index, first, last, pageCount } = pathContext;
