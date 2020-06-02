cecc6236e3e4cd22fac3ad48b3e3fde5ee17f734
Hao Dong <dong_hao@hotmail.co.uk>
2020-06-02 19:20:22 +0100

    Fix build, move to dart-sass to avoid Ruby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,23 +1,24 @@
-require('dotenv').config()
+require("dotenv").config();
 
 module.exports = {
     siteMetadata: {
         siteUrl: `https://haodong.io`,
-        author: 'Hao Dong',
-        title: 'Hao\'s learning log',
-        description: 'Development, UX, Programming Languages, Frameworks, Libraries, experiences and anything random.'
+        author: "Hao Dong",
+        title: "Hao's learning log",
+        description:
+            "Development, UX, Programming Languages, Frameworks, Libraries, experiences and anything random.",
     },
     plugins: [
         {
-            resolve: 'gatsby-source-contentful',
+            resolve: "gatsby-source-contentful",
             options: {
-                spaceId: process.env.CONTENTFUL_SPACE_ID || '',
-                accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || '',
-                host: process.env.CONTENTFUL_HOST || 'cdn.contentful.com'
+                spaceId: process.env.CONTENTFUL_SPACE_ID || "",
+                accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || "",
+                host: process.env.CONTENTFUL_HOST || "cdn.contentful.com",
             },
         },
         {
-            resolve: `gatsby-plugin-sitemap`
+            resolve: `gatsby-plugin-sitemap`,
         },
         {
             resolve: `gatsby-plugin-feed`,
@@ -34,22 +35,27 @@ module.exports = {
                         }
                     }
                 `,
-                feeds: [{
-                    serialize: ({ query: { site, allContentfulBlogPost } }) => {
-                        return allContentfulBlogPost.edges.map(edge => {
-                            // TODO: remove published and updated
+                feeds: [
+                    {
+                        serialize: ({
+                            query: { site, allContentfulBlogPost },
+                        }) => {
+                            return allContentfulBlogPost.edges.map((edge) => {
+                                // TODO: remove published and updated
 
-                            return {
-                                title: edge.node.title,
-                                url: `${site.siteMetadata.siteUrl}/${edge.node.slug}`,
-                                guid: `${site.siteMetadata.siteUrl}/${edge.node.slug}`,
-                                description: edge.node.description.childMarkdownRemark.html,
-                                published: edge.node.publishDate,
-                                updated: edge.node.updatedAt
-                            };
-                        });
-                    },
-                    query: `
+                                return {
+                                    title: edge.node.title,
+                                    url: `${site.siteMetadata.siteUrl}/${edge.node.slug}`,
+                                    guid: `${site.siteMetadata.siteUrl}/${edge.node.slug}`,
+                                    description:
+                                        edge.node.description
+                                            .childMarkdownRemark.html,
+                                    published: edge.node.publishDate,
+                                    updated: edge.node.updatedAt,
+                                };
+                            });
+                        },
+                        query: `
                             {
                                 allContentfulBlogPost(
                                     sort: { fields: [publishDate], order: DESC },
@@ -71,25 +77,31 @@ module.exports = {
                                 }
                             }
                         `,
-                    output: '/rss.xml',
-                }]
-            }
+                        output: "/rss.xml",
+                    },
+                ],
+            },
         },
         {
             resolve: `gatsby-plugin-manifest`,
             options: {
-                name: 'Hao\'s learning log',
-                short_name: 'Hao\'s blog',
-                start_url: '/',
-                background_color: '#fff',
-                theme_color: '#555',
-                display: 'minimal-ui',
-                icon: 'src/assets/icon.svg', // This path is relative to the root of the site.
+                name: "Hao's learning log",
+                short_name: "Hao's blog",
+                start_url: "/",
+                background_color: "#fff",
+                theme_color: "#555",
+                display: "minimal-ui",
+                icon: "src/assets/icon.svg", // This path is relative to the root of the site.
             },
         },
-        'gatsby-plugin-react-helmet',
-        'gatsby-transformer-remark',
-        'gatsby-plugin-sass',
+        "gatsby-plugin-react-helmet",
+        "gatsby-transformer-remark",
+        {
+            resolve: `gatsby-plugin-sass`,
+            options: {
+                implementation: require("dart-sass"),
+            },
+        },
         `gatsby-plugin-remove-trailing-slashes`,
         {
             resolve: `gatsby-transformer-remark`,
@@ -101,7 +113,7 @@ module.exports = {
                             classPrefix: "language-",
                             inlineCodeMarker: null,
                             aliases: {
-                                js: 'javascript',
+                                js: "javascript",
                             },
                             showLineNumbers: false,
                             noInlineHighlight: false,
