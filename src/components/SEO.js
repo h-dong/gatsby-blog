444d4c18c5fc00c4d3d3c21a1a187ec80e50a9e6
Hao Dong <dong_hao@hotmail.co.uk>
2020-06-10 14:48:24 +0100

    Removed debugging comments

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -11,83 +11,80 @@ const SEO = ({ siteMetadata, title, description, pathname, image }) => {
     const canonical = pathname ? `${siteMetadata.siteUrl}/${pathname}` : null;
 
     return (
-        <div>
-            {JSON.stringify(metaDescription)}
-            <Helmet
-                title={title}
-                titleTemplate={`%s | ${siteMetadata.title}`}
-                link={
-                    canonical
-                        ? [
-                              {
-                                  rel: "canonical",
-                                  href: canonical,
-                              },
-                          ]
-                        : []
-                }
-                meta={[
-                    {
-                        name: `description`,
-                        content: metaDescription,
-                    },
-                    {
-                        name: "keywords",
-                        content: siteMetadata.keywords,
-                    },
-                    {
-                        property: `og:title`,
-                        content: title,
-                    },
-                    {
-                        property: `og:description`,
-                        content: metaDescription,
-                    },
-                    {
-                        property: `og:type`,
-                        content: `website`,
-                    },
-                    {
-                        name: `twitter:creator`,
-                        content: siteMetadata.author,
-                    },
-                    {
-                        name: `twitter:title`,
-                        content: title,
-                    },
-                    {
-                        name: `twitter:description`,
-                        content: metaDescription,
-                    },
-                ].concat(
-                    metaImage
-                        ? [
-                              {
-                                  property: "og:image",
-                                  content: image,
-                              },
-                              {
-                                  property: "og:image:width",
-                                  content: metaImage.width,
-                              },
-                              {
-                                  property: "og:image:height",
-                                  content: metaImage.height,
-                              },
-                              {
-                                  name: "twitter:card",
-                                  content: "summary_large_image",
-                              },
-                          ]
-                        : [
-                              {
-                                  name: "twitter:card",
-                                  content: "summary",
-                              },
-                          ]
-                )}
-            />
-        </div>
+        <Helmet
+            title={title}
+            titleTemplate={`%s | ${siteMetadata.title}`}
+            link={
+                canonical
+                    ? [
+                          {
+                              rel: "canonical",
+                              href: canonical,
+                          },
+                      ]
+                    : []
+            }
+            meta={[
+                {
+                    name: `description`,
+                    content: metaDescription,
+                },
+                {
+                    name: "keywords",
+                    content: siteMetadata.keywords,
+                },
+                {
+                    property: `og:title`,
+                    content: title,
+                },
+                {
+                    property: `og:description`,
+                    content: metaDescription,
+                },
+                {
+                    property: `og:type`,
+                    content: `website`,
+                },
+                {
+                    name: `twitter:creator`,
+                    content: siteMetadata.author,
+                },
+                {
+                    name: `twitter:title`,
+                    content: title,
+                },
+                {
+                    name: `twitter:description`,
+                    content: metaDescription,
+                },
+            ].concat(
+                metaImage
+                    ? [
+                          {
+                              property: "og:image",
+                              content: image,
+                          },
+                          {
+                              property: "og:image:width",
+                              content: metaImage.width,
+                          },
+                          {
+                              property: "og:image:height",
+                              content: metaImage.height,
+                          },
+                          {
+                              name: "twitter:card",
+                              content: "summary_large_image",
+                          },
+                      ]
+                    : [
+                          {
+                              name: "twitter:card",
+                              content: "summary",
+                          },
+                      ]
+            )}
+        />
     );
 };
 
