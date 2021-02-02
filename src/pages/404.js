96d09a2bf239f3162d66bdc1bc3ff45e351c520a
Hao Dong <dong_hao@hotmail.co.uk>
2021-02-02 19:57:56 +0000

    Add page not found SEO data

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,17 @@
-import React from 'react'
-import Layout from "../components/layout"
+import React from "react";
+import Layout from "../components/layout";
 
 const NotFoundPage = () => (
     <Layout>
+        <SEO title={"404"} />
         <div className="page">
             <h1 className="page-title">PAGE NOT FOUND</h1>
-            <p>The page you are trying to visit doesn&#39;t exist... the sadness.</p>
+            <p>
+                The page you are trying to visit doesn&#39;t exist... the
+                sadness.
+            </p>
         </div>
     </Layout>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
